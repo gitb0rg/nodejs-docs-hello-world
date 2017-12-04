a4066242d1b4620f817e17a53f915aa95066c9fb
GITb0rg <33669350+gitb0rg@users.noreply.github.com>
2017-12-04 20:57:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("ITAcademy Vasiliy COMPLETE!");
+    response.end("ITAcademy Vasiliy COMPLETE!!");
 
 });
 
